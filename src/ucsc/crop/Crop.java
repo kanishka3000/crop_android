17fc8a4a2e0dc5216b531795ce57b84716595bd5
Kanishka_ <Kanishka_@kanishkaw>
2010-12-07 20:27:43 +0530

    s

--- src/ucsc/crop/Crop.java
+++ src/ucsc/crop/Crop.java
@@ -18,15 +18,13 @@ public class Crop {
 	public static final int GET_FROM_CROP = 1;
 	public static final int GET_FROM_LOCATION = 0;
 	String URL = "http://112.135.95.142:8080/myaxis/getCropService?";
-public String Location;
-public String Price;
-public String Name;
+	public String Location;
+	public String Price;
+	public String Name;
 	public Crop() {
 
 	}
-
 	public List getCrops(int type, String value) throws Exception {
-		
 		if (type == GET_FROM_CROP) {
 			URL += "service=crop&id=" + value;
 		} else if (type == GET_FROM_LOCATION) {
@@ -42,25 +40,25 @@ public String Name;
 	private List<Crop> parseXml(InputStream cropString)
 			throws XmlPullParserException, IOException {
 		KXmlParser parse = new KXmlParser();
-		List<Crop> croplist=new ArrayList<Crop>();
+		List<Crop> croplist = new ArrayList<Crop>();
 		parse.setInput(new BufferedReader(new InputStreamReader(cropString)));
 		parse.nextTag();
 		parse.require(XmlPullParser.START_TAG, null, "crops");
 		while (parse.nextTag() == XmlPullParser.START_TAG) {
-			Crop crop=new Crop();
+			Crop crop = new Crop();
 			parse.require(XmlPullParser.START_TAG, null, "crop");
 			parse.nextTag();
 			String location = parse.nextText();
-			crop.Location=location;
+			crop.Location = location;
 			parse.nextTag();
 			String price = parse.nextText();
-			crop.Price=price;
+			crop.Price = price;
 			parse.nextTag();
 			String name = parse.nextText();
-			crop.Name=name;
+			crop.Name = name;
 			parse.nextTag();
 			croplist.add(crop);
-			parse.require(XmlPullParser.END_TAG, null, "crop");		
+			parse.require(XmlPullParser.END_TAG, null, "crop");
 		}
 		return croplist;
 	}
