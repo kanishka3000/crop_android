2dc09e08ddbb2300ea9a69d3880546e63fa69885
Kanishka <Kanishka@192.168.1.2>
2010-12-23 08:16:01 +0530

    comit

--- src/ucsc/crop/ui/LocationSelection.java
+++ src/ucsc/crop/ui/LocationSelection.java
@@ -60,7 +60,7 @@ public class LocationSelection extends Activity {
 								.sendMessage(fetchAndDisplayLocationData
 										.obtainMessage());
 					} catch (Exception e) {
-						// TODO Auto-generated catch block
+						
 						e.printStackTrace();
 					}
 
---
+++ src/ucsc/crop/ui/PriceSelection.java
@@ -0,0 +1,5 @@
+package ucsc.crop.ui;
+
+public class PriceSelection {
+
+}
